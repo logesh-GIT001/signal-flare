3e10c461b6977a58e0215ff55de311c98399e5ef
logesh-GIT001 <logesh.spy@gmail.com>
2025-12-27 12:06:18 +0530

    feat: export FlareGenerator in package init

--- cli/signal_flare/__init__.py
+++ cli/signal_flare/__init__.py
@@ -5,3 +5,10 @@ SIGNAL-FLARE: Post-Exploitation Breach Confirmation
 __version__ = "1.1.0"
 __author__ = "SIGNAL-FLARE Contributors"
 __license__ = "MIT"
+
+
+# Import so users can do: from signal_flare import FlareGenerator
+
+from .generator import FlareGenerator
+
+__all__ = ["FlareGenerator", "__version__"]
